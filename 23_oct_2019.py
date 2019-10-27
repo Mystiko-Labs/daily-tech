6d5a1a1e972cf4a8b70eaf9de2be9e9faa51d56d
Cartmanishere <apbetahouse45@gmail.com>
2019-10-27 17:34:41 +0530

    corrected wrong answer and added more test cases. this ans accepted on leetcode

--- 23_oct_2019.py
+++ 23_oct_2019.py
@@ -2,15 +2,6 @@
 Question:
 Given a string, find the length of the longest substring without repeating characters
 
-Idea:
-> Iterate over the characters in the given string one by one.
-> Keep a count of how many characters iterated.
-> If a character is repeated, compare the previous max length with current length of substring and keep the maximum
-> Reset the counter when character is repeated and start counting again.
-> At the end, return the current count or previous max length
-
-Time Complexity => O(n)
-
 Feel free to test with more corner cases and raise an issue in case of a bug
 """
 
@@ -26,43 +17,44 @@ class Solution():
         lmax = 0
 
         counter = 0
+        sstart = 0
         for index, char in enumerate(s):
+
             try:
-                if mdict[char]:
-                    lmax = max(lmax, counter)
-                    mdict = {}
-                    mdict[char] = True
-                    counter = 1
+                previdx = mdict[char]
+                mdict[char] = index
+                counter = index - max(previdx, sstart-1)
+                print(' ', max(previdx, sstart), end='')
+                sstart = max(sstart, previdx + 1)
 
             except KeyError as e:
-                mdict[char] = True
+                mdict[char] = index
                 counter += 1
 
+            lmax = max(lmax, counter)
+            print('|', char, index, counter, lmax, mdict, sstart)
+
         return max(lmax, counter)
+
 
 class TestCases(unittest.TestCase):
     def setUp(self) -> None:
         self.sol = Solution()
 
-    def test_case1(self):
+    def test_cases(self):
         self.assertEqual(self.sol.lengthOfLongestSubstring('abcdefghijklmnopqrstuvwxyz'), 26)
-
-    def test_case2(self):
         self.assertEqual(self.sol.lengthOfLongestSubstring('abcddef'), 4)
-
-    def test_case3(self):
         self.assertEqual(self.sol.lengthOfLongestSubstring('aabcdef'), 6)
-
-    def test_case4(self):
         self.assertEqual(self.sol.lengthOfLongestSubstring('aabbcdefghhijk'), 7)
-
-    def test_case5(self):
         self.assertEqual(self.sol.lengthOfLongestSubstring('aa'), 1)
-
-    def test_case6(self):
         self.assertEqual(self.sol.lengthOfLongestSubstring('ab'), 2)
+        self.assertEqual(self.sol.lengthOfLongestSubstring('dvdf'), 3)
+        self.assertEqual(self.sol.lengthOfLongestSubstring('tmmzuxt'), 5)
+        self.assertEqual(self.sol.lengthOfLongestSubstring('abcabcbb'), 3)
+        self.assertEqual(self.sol.lengthOfLongestSubstring('wobgrovw'), 6)
+        self.assertEqual(self.sol.lengthOfLongestSubstring('zwnigfunjwz'), 8)
 
 
 if __name__=="__main__":
-    ans = Solution().lengthOfLongestSubstring('aabcdef')
+    ans = Solution().lengthOfLongestSubstring("zwnigfunjwz")
     print(ans)
